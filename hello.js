506e6df2bd63107da57187f4d10fcf25bc6f7cc8
Mukund Harikumar <mukundhk.2010@gmail.com>
2022-11-27 08:46:12 +0000

    Create 4 files

---
+++ hello.js
@@ -0,0 +1,15 @@
+// null is given by programmer
+// undefined is given by program
+
+// map syntax
+// iterable.map(function)
+
+const cowsay = require("cowsay");
+
+console.log(
+  cowsay.say({
+    text: "I'm a moooodule",
+    e: "👁👁",
+    T: "🦷🦷",
+  })
+);
